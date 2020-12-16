d2114fa449c113418f8475cc53e1bc0f22c8739d
student <you@example.com>
2020-12-16 14:26:25 +0100

    Verze 0.4

--- src/main.cpp
+++ src/main.cpp
@@ -3,61 +3,50 @@
 #define LEDP 5
 #define LEDL 6
 const int MPU = 0x68; // MPU6050 I2C address
-float AccX;
-float GyroX;
-float accAngleX, gyroAngleX;
-float roll;
+float AccX, GyroX, accAngleX, gyroAngleX;
+float roll, rollTmp, avRoll;
 float AccErrorX, GyroErrorX;
 float elapsedTime, currentTime, previousTime;
 int c = 0;
+float p[10];
 unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 500;
 const int lP = 2;
 const int pP = 3;
+int i = 0;
 boolean pS = false;
 boolean lS = false;
 boolean vS = false;
-void podminka(char s){
-  Serial.println(s);
-  if(s=='l'&&vS==false){
-    pS=false;
-    if(lS==true){
-      lS=false;
-    }
-    else{
-      lS=true;
-    }
-  }
-  if(s=='p'&&vS==false){
-    lS=false;
-    if(pS==true){
-      pS=false;
-    }
-    else{
-      pS=true;
-    }
-  }
-  return;
-}
+boolean state = false;
 
 void zhasni(){
   digitalWrite(LEDP, LOW);
   digitalWrite(LEDL, LOW);
 }
 
-void rozsvit(int p){
-  digitalWrite(p, HIGH);
+void podminka(char s){
+  zhasni();
+  if(s=='l'&&vS==false){
+    pS=false;
+    lS=!lS;
+  }
+  if(s=='p'&&vS==false){
+    lS=false;
+    pS=!pS;
+  }
+  return;
 }
+/*void rozsvit(int p){
+  digitalWrite(p, HIGH);
+}*/
 
 void pressInterruptP(){
   podminka('p');
-  zhasni();
 }
 
 void pressInterruptL(){
   podminka('l');
-  zhasni();
 }
 
 void setup(){
@@ -83,17 +72,21 @@ void loop(){
     if(lS){
       digitalWrite(LEDL, !digitalRead(LEDL));  //if so, change the state of the LED.  Uses a neat trick to change the state
     }
+    if(lS||pS){
+
+    }
     startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
-    Serial.println(roll);
+    roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
+    Serial.println(rollTmp);
   }
-    Wire.beginTransmission(MPU);
+  Wire.beginTransmission(MPU);
   Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
   //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
   AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
   // Calculating Roll from the accelerometer data
-  //accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI); // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
+  //accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI); // AccErrorX ~(0) See the calculate_IMU_error()custom function for more details
   // === Read gyroscope data === //
   previousTime = currentTime;        // Previous time is stored before the actual time read
   currentTime = millis();            // Current time actual time read
@@ -104,11 +97,10 @@ void loop(){
   Wire.requestFrom(MPU, 6, true); // Read 4 registers total, each axis value is stored in 2 registers
   GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
   // Correct the outputs with the calculated error values
-  GyroX = GyroX+2.93 ; // GyroErrorX ~(-0.56)
+  GyroX = GyroX+2.93 ; // GyroErrorX ~(-2.93)
   // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
   gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
   // Complementary filter - combine acceleromter and gyro angle values
   roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
   // Print the values on the serial monitor
-  
 }
